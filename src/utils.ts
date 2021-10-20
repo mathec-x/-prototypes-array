dc23d72117a9d62e3ee705d93a082c1fbd537143
matheus correa <mathec@live.com>
2021-10-20 12:32:55 -0300

    fix: search object

--- src/utils.ts
+++ src/utils.ts
@@ -3,10 +3,10 @@ const ReplaceGexp = new RegExp(/[\u0300-\u036f]/, 'g');
 export const Normalized = (text: String) : string => text && String(text).trim().normalize("NFD").replace(ReplaceGexp, "").toLowerCase();
 
 export const MakeDeepSearch = (part : string, keys: string[], obj: any) => keys.some( 
-    (key) => typeof obj === 'string' ? Normalized(obj).indexOf( Normalized(part) ) > -1
-            : typeof obj[key] === 'object' ? MakeDeepSearch(part, Object.keys(obj[key]), obj[key])
-            : typeof obj[key] === 'number' ? MakeDeepSearch(part, keys, String(obj[key])) 
-            : typeof obj[key] === 'string' ? MakeDeepSearch(part, keys, String(obj[key]))
+    (key) => obj && typeof obj === 'string' ? Normalized(obj).indexOf( Normalized(part) ) > -1
+            : obj && obj[key] && typeof obj[key] === 'object' ? MakeDeepSearch(part, Object.keys(obj[key]), obj[key])
+            : obj && obj[key] && typeof obj[key] === 'number' ? MakeDeepSearch(part, keys, String(obj[key])) 
+            : obj && obj[key] && typeof obj[key] === 'string' ? MakeDeepSearch(part, keys, String(obj[key]))
             : false
 )
 
