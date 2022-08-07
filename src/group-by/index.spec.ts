0af474e100b422e7966de318e7ccaaef9f13a13b
matheus correa <mathec@live.com>
2022-08-07 14:44:29 -0300

    feat: implements instance functions for group by

--- src/group-by/index.spec.ts
+++ src/group-by/index.spec.ts
@@ -3,27 +3,46 @@ import './index'
 const array = [
     { data: '2022-01-01', id: 1, qtde_inf: 1, qtde_pass: 1 },
     { data: '2022-01-02', id: 1, qtde_inf: 2, qtde_pass: 1 },
-    { data: '2022-02-01', id: 1, qtde_inf: 4, qtde_pass: 1 },
+    { data: '2022-02-01', id: 1, qtde_inf: 4, qtde_pass: 1, latitude: 80, longitude: -80 },
     { data: '2022-02-02', id: 1, qtde_inf: 4, qtde_pass: 1 },
     { data: '2022-02-03', id: 1, qtde_inf: 3, qtde_pass: 1 },
-    { data: '2022-02-03', id: 2, qtde_inf: 4, qtde_pass: 1 },
-    { data: '2022-02-04', id: 2, qtde_inf: 4, qtde_pass: 1 },
-    { data: '2022-02-05', id: 2, qtde_inf: 4, qtde_pass: 1 },
-    { data: '2022-02-06', id: 2, qtde_inf: 4, qtde_pass: 1 },
+    { data: '2022-02-03', id: 2, qtde_inf: 23, qtde_pass: 1 },
+    { data: '2022-02-04', id: 2, qtde_inf: 44, qtde_pass: 1 },
+    { data: '2022-02-05', id: 2, qtde_inf: 45, qtde_pass: 1, latitude: -100, longitude: 100 },
+    { data: '2022-02-06', id: 2, qtde_inf: 46, qtde_pass: 1, latitude: -200, longitude: 200 },
 ];
 
-const increment = (cur: number, acc = 0) => cur + acc;
 describe('Array.GroupBy', () => {
-    it('Should Gourp by date and id', () => {
+    it('Should group by date', () => {
         const result = array.GroupBy(
-        // ['id'],
-        (x) => x.data.substring(0, 7) + x.id,
-        (current, next ) => {
-            return {
-                id: current.id,
-                violation: increment(current.qtde_inf, next?.violation)
-            }}
+            // ['id'],
+            (x) => x.data.substring(0, 7) + x.id,
+            (current, next) => {
+                return {
+                    id: current.id,
+                    violation: next.Sum(current.qtde_inf, next.violation),
+                    max: next.Max(current.qtde_inf, next.max),
+                    min: next.Min(current.qtde_inf, next.min),
+                    dates: next.ConcatArray(current.data, next.dates),
+                    coords: next.AorB({ 
+                        latitude: current.latitude, 
+                        longitude: current.longitude 
+                    }, next.coords ),
+                    data: next.Has('data')
+                }
+            }
         )
+
+        console.log(result)
         expect(result).toBeInstanceOf(Array);
+        expect(result[0]).toMatchObject({
+            id: 1,
+            violation: 3,
+            max: 2,
+            min: 1,
+            dates: [ '2022-01-01', '2022-01-02' ],
+            coords: {},
+            data: false    
+        });
     })
 })
--- src/group-by/index.ts
+++ src/group-by/index.ts
@@ -1,17 +1,23 @@
 export { }
 type FuncOrArgs<T> = Partial<keyof T>[] | ((current: T, next: T) => any)
 
-declare global {
-  interface Array<T> {
-      GroupBy<Result>(
-          getkey: FuncOrArgs<T>,
-          callback: <Next = { [P in keyof Result]: Result[P] } >(previous: T, next?: Next) => Result
-      ): Result[]
-  }
+/**
+ * @todo
+ * i need the callback of this function to be the same type as its return
+ */
+ declare global {
+    interface Array<T> {
+        GroupBy<Result = { [k: string]: any }, Res = Result>(
+            getkey: FuncOrArgs<T>,
+            callback: <Prev extends T, Next extends NewInstance<Res>>(
+                previous: Prev, next: Next
+            ) => Result
+        ): Result[]
+    }
 }
 
 const mountKey = (key: any, object: any) => {
-    if(typeof key === 'function'){
+    if (typeof key === 'function') {
         return key(object);
     } else {
         let res = '';
@@ -22,23 +28,60 @@ const mountKey = (key: any, object: any) => {
     }
 }
 
+type NewInstance<T> = InstanceOfGroupBy<T> & {} & { [k: string]: any }
+
+class InstanceOfGroupBy<T = any> {
+    constructor(params: T) {
+        Object.assign(this, params)
+    }
+
+    Has(a: keyof T) {
+        return !!(this as any)?.[a]
+    }
+
+    True<Value, Into>(a: Value, b: Into | any = undefined) {
+        return !b ? a : false
+    }
+    ConcatArray<Value>(a: Value | undefined = undefined, b: any[] | undefined = undefined): Value[] {
+        return !b ? [a] : b.concat(a) as any
+    }
+    AorB<Value extends any>(a: Value, b: Value | any = undefined): Value {
+        if (a && a instanceof Object) {
+            for (const k in a) {
+                if (Object.prototype.hasOwnProperty.call(a, k)) {
+                    if (!(a as any)[k]) { return b || {} }
+                }
+            }
+        }
+
+        return a || b
+    }
+    Max<Value, Into>(a: Value | any = 0, b: Into | any = 0) {
+        return Number(a) > Number(b) ? Number(a) : Number(b)
+    }
+    Min<Value, Into>(a: Value, b: Into | any = a) {
+        return Number(a) <= Number(b) ? Number(a) : Number(b)
+    }
+    Sum<Value, Into>(a: Value | any = 0, b: Into | any = 0) {
+        return Number(a) + Number(b)
+    }
+}
+
 if (!Array.prototype.GroupBy) {
     Object.defineProperty(Array.prototype, 'GroupBy', {
         value: function GroupBy<T>(
-            this: Array<T>, 
-            getkey: FuncOrArgs<T>, 
-            callback: <Next = Partial<T>>(current: T, next?: Next) => T) {
-            const result = this.reduce((res: any, currentValue: T) => {
-                const index = mountKey(getkey, currentValue);
-                if(!res[index]){
-                    res[index] = callback(currentValue, res[index]);
-                } else {
-                    res[index] = callback(currentValue, res[index]);
-                }
-                return res;
-            }, {});
-
-            return Object.values(result);
+            this: Array<T>,
+            getkey: FuncOrArgs<T>,
+            callback: (current: T, next: any) => T) {
+            let index: any, acc: any;
+            return Object.values(
+                this.reduce((res: any, currentValue) => {
+                    index = mountKey(getkey, currentValue);
+                    acc = new InstanceOfGroupBy(res[index])
+                    res[index] = callback(currentValue, acc);
+                    return res;
+                }, {})
+            );
         }
     })
 }
